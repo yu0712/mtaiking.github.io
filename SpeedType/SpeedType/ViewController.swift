46dfc5ee0a1704b52db150f34820d7c8a919e044
mtaiking <blackcatpurapura@icloud.com>
2015-07-19 13:32:06 +0900

    radio

---
+++ SpeedType/SpeedType/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SpeedType
+//
+//  Created by 三池 大樹 on 2015/07/17.
+//  Copyright © 2015年 三池 大樹. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ SpeedType/SpeedTypeUITests/SpeedTypeUITests.swift
@@ -0,0 +1,34 @@
+//
+//  SpeedTypeUITests.swift
+//  SpeedTypeUITests
+//
+//  Created by 三池 大樹 on 2015/07/17.
+//  Copyright © 2015年 三池 大樹. All rights reserved.
+//
+
+import XCTest
+
+class SpeedTypeUITests: XCTestCase {
+        
+    override func setUp() {
+        super.setUp()
+        
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+        
+        // In UI tests it is usually best to stop immediately when a failure occurs.
+        continueAfterFailure = false
+        // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
+        XCUIApplication().launch()
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // Use recording to get started writing UI tests.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+}
